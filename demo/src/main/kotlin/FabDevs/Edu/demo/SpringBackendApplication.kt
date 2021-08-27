337ad5925d53b3ea7165e8fc40d6482bda823cde
akshat <20akshatsahijpal01droid01@gmail.com>
2021-08-28 00:43:42 +0530

    v1

---
+++ demo/src/main/kotlin/FabDevs/Edu/demo/SpringBackendApplication.kt
@@ -0,0 +1,11 @@
+package FabDevs.Edu.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBackendApplication>(*args)
+}
---
+++ demo/src/test/kotlin/FabDevs/Edu/demo/SpringBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package FabDevs.Edu.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
