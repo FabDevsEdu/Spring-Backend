05f142a70b95c8ce18e3e0757416a20c54e85d2d
akshat <20akshatsahijpal01droid01@gmail.com>
2021-08-28 17:01:59 +0530

    v1

--- demo/src/main/kotlin/FabDevs/Edu/demo/repository/PostCrudRepository.kt
+++ demo/src/main/kotlin/FabDevs/Edu/demo/repository/PostCrudRepository.kt
@@ -3,8 +3,6 @@ package FabDevs.Edu.demo.repository
 import FabDevs.Edu.demo.data.PostData
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
-import java.util.*
 
 @Repository
-interface PostCrudRepository: JpaRepository<PostData, UUID> {
-}
+interface PostCrudRepository : JpaRepository<PostData, Int>
--- demo/src/main/kotlin/FabDevs/Edu/demo/service/PostServiceImpl.kt
+++ demo/src/main/kotlin/FabDevs/Edu/demo/service/PostServiceImpl.kt
@@ -1,6 +1,37 @@
 package FabDevs.Edu.demo.service
 
+import FabDevs.Edu.demo.data.PostData
+import FabDevs.Edu.demo.repository.PostCrudRepository
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
-interface PostServiceImpl: PostService {}
+class PostServiceImpl @Autowired constructor(var repo: PostCrudRepository) : PostService {
+    override fun savePost(postData: PostData): PostData {
+        return repo.save(postData)
+    }
+
+    override fun fetchAllPosts(): List<PostData> {
+        return repo.findAll()
+    }
+
+    override fun getPostWithThisName(name: Int): Optional<PostData> {
+        return repo.findById(name)
+    }
+
+    override fun deleteThisId(id: Int) {
+        repo.deleteById(id)
+    }
+
+    override fun updateThisPost(postId: Int, post: PostData): PostData {
+        var remote = repo.findById(postId).get()
+        when{
+            Objects.nonNull(post.postAuthor) -> remote.postAuthor = post.postAuthor
+            Objects.nonNull(post.postContent) -> remote.postContent = post.postContent
+            Objects.nonNull(post.postDate) -> remote.postDate = post.postDate
+            Objects.nonNull(post.postImage) -> remote.postImage = post.postImage
+        }
+        return repo.save(remote)
+    }
+}
