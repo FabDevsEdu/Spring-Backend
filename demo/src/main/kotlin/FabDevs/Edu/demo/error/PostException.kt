467bf1616c18a708b6ad373e279790051abd450e
akshat <20akshatsahijpal01droid01@gmail.com>
2021-08-29 22:33:27 +0530

    Updated error package

--- demo/src/main/kotlin/FabDevs/Edu/demo/error/PostException.kt
+++ demo/src/main/kotlin/FabDevs/Edu/demo/error/PostException.kt
@@ -1,3 +1,43 @@
 package FabDevs.Edu.demo.error
 
-class PostException: Exception()
+import java.io.PrintStream
+import java.io.PrintWriter
+
+class PostException: Exception(){
+    override fun getLocalizedMessage(): String {
+        return super.getLocalizedMessage()
+    }
+
+    override fun initCause(cause: Throwable?): Throwable {
+        return super.initCause(cause)
+    }
+
+    override fun fillInStackTrace(): Throwable {
+        return super.fillInStackTrace()
+    }
+
+    override fun getStackTrace(): Array<StackTraceElement> {
+        return super.getStackTrace()
+    }
+
+    override fun setStackTrace(stackTrace: Array<out StackTraceElement>?) {
+        super.setStackTrace(stackTrace)
+    }
+
+    override val cause: Throwable?
+        get() = super.cause
+    override val message: String?
+        get() = super.message
+
+    override fun printStackTrace() {
+        super.printStackTrace()
+    }
+
+    override fun printStackTrace(s: PrintStream?) {
+        super.printStackTrace(s)
+    }
+
+    override fun printStackTrace(s: PrintWriter?) {
+        super.printStackTrace(s)
+    }
+}
